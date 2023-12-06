ebbdbecedf0c2d85c5392c695d61e9aa175253a7
Alysha Reinard <alysha.reinard@gmail.com>
2023-12-06 13:55:18 +0100

    checking if form actions work on live site7

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,79 +1,92 @@
 import type { Login } from '$lib/interfaces/login';
-export async function load({ fetch}){
-    const response = await fetch('/api/getUsers', {
-        method: 'GET',
-        body: null,
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    const value = await response.json();
-    const familyMembers = value.filter((member:Login) => member.valid==true);
-    return{
-        familyMembers
-    }
+import { tick } from 'svelte';
+export async function load({ fetch }) {
+	const response = await fetch('/api/getUsers', {
+		method: 'GET',
+		body: null,
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+	const value = await response.json();
+	const familyMembers = value.filter((member: Login) => member.valid == true);
+	return {
+		familyMembers
+	};
 }
 
-function checkPassword(password:string, user:Login) {
-    let message:string=''
-    if (password == user.password) {
+async function checkPassword(password: string, userid: string) {
+    console.log("in checkpassword")
+	let message: string = '';
+	let user: Login;
+	const response = await fetch('/api/getUsers', {
+		method: 'GET',
+		body: null,
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
 
-        const userRecord=getUserRecord(user.id);
-        message="Success";
-        return({
-            message:message,
-            userRecord
-        })
-    } else {
-        return({
-            message:'Password incorrect'
-        })
-       
-    }
+	const value = await response.json();
+
+	for (let i = 0; i < value.length; i++) {
+		if (value[i].id == userid) {
+			user = value[i];
+            console.log("just found ", user)
+			break;
+		}
+	}
+	await tick;
+
+	if (user && password == user.password) {
+		const userRecord = getUserRecord(user.id);
+		message = 'Success';
+		return {
+			message: message,
+			userRecord
+		};
+	} else {
+		return {
+			message: 'Password incorrect'
+		};
+	}
 }
-
 
 async function getUserRecord(userid: string) {
-    console.log('Starting getUserRecord');
-    const response = await fetch('/api/getUserRecord?userid=' + userid, {
-        method: 'GET',
-        body: null,
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    const value = await response.json();
-    console.log('value from create is ', value);
-    return(value)
-
+	console.log('Starting getUserRecord');
+	const response = await fetch('/api/getUserRecord?userid=' + userid, {
+		method: 'GET',
+		body: null,
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+	const value = await response.json();
+	console.log('value from create is ', value);
+	return value;
 }
-
 
 export const actions = {
 	default: async ({ request }) => {
-        console.log("We're in actions")
+		console.log("We're in actions");
 
-        const data = await request.formData();
-        const user = data.get('user');
-        const member = data.get('member');
-        const password = data.get('password');
-        console.log("user is ", user);
-        console.log("password is ", password)
-        console.log("member is ", member)
+		const data = await request.formData();
+		const userid = data.get('user');
+		const password = data.get('password');
+		console.log('user is ', userid);
 
-/*
-        if (password == user.password) {
-			localStorage.setItem('userid', user.id);
-			localStorage.setItem('userName', user.name);
-			getUserRecord(user.id);
+		if (!userid || !password) {
+			return {
+				message: 'Please enter a user id and password'
+			};
 		} else {
-			message = 'password incorrect';
-		}*/
+			const response = await checkPassword(password, userid);
 
-		return {
-            message: "Success",
-  
-			//userRecord
-		};
-    }
-}
+			return {
+				message: response.message,
+				userRecord: response.userRecord
+				//userRecord
+			};
+		}
+	}
+};
